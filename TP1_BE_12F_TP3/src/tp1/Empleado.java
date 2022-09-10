c4aa9d41a286f10f9999ca1de4a35745d0fbd7d2
Juan Vidal <juanignaciovidal.1993@gmail.com>
2022-09-11 03:16:25 +1100

    agrego cosas

--- TP1_BE_12F_TP3/src/tp1/Empleado.java
+++ TP1_BE_12F_TP3/src/tp1/Empleado.java
@@ -1,5 +1,16 @@
 package tp1;
 
 public class Empleado {
-
+	
+	private String nombre;
+	private int edad;
+	
+	
+	public Empleado(String nombre, int edad) {
+		
+		this.nombre = nombre;
+		this.edad = edad;
+	}
+	
+	
 }
